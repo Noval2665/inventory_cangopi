c4b0119ebd0735d9ec048e6ef4d69d94270bbaba
dmitriy-ulyanov <daviderinugroho@gmail.com>
2024-03-04 12:45:56 +0700

    penambahan function sementara pada purchase report controller, mintol dicek ghaess awokawoka

--- app/Http/Controllers/PurchaseReportController.php
+++ app/Http/Controllers/PurchaseReportController.php
@@ -2,16 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MarketList;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class PurchaseOrderDetailController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $page = $request->page;
+        $per_page = $request->per_page ?? 10000;
+        $search = $request->search;
+
+        $report = PurchaseReport::when($search, function ($query, $search) {
+            return $query->where('dates', 'LIKE', '%' . $search . '%');
+        })
+            ->paginate($per_page, ['*'], 'page', $page);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Menampilkan data laporan penjualan',
+            'categories' => $purchase_order,
+        ], 200);
     }
 
     /**
@@ -27,7 +45,34 @@ class PurchaseOrderDetailController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            '' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response([
+                'status' => 'error',
+                'errors' => $validator->errors(),
+                'message' => $validator->errors()->first(),
+            ], 422);
+        }
+
+        $report = PurchaseReport::create([
+            'item_name' => ucwords($request->item_name),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        if (!$createReport) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Gagal menambah laporan penjualan',
+            ], 400);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Berhasil menambah laporan penjualan',
+        ], 201);
     }
 
     /**
