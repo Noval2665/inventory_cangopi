6645c033bd3d463d2aef25e846941eaa93b03b17
JulianYang345 <126272283+JulianYang345@users.noreply.github.com>
2024-04-22 11:56:44 +0700

    fix/master-data-supplier

--- database/migrations/2024_02_20_225526_create_suppliers_table.php
+++ database/migrations/2024_02_20_225526_create_suppliers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('supplier_name');
             $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
             $table->boolean('is_active')->default(true);
             $table->foreignId('user_id');
 
